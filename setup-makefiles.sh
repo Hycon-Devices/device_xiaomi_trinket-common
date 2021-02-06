779fe9d9b36825c2cadbb80e814676fa2b9bb91b
madmax7896 <thegreatsagar123@gmail.com>
2021-02-06 09:02:59 +0000

    trinket-common: Hycon bringup

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -12,9 +12,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-EVOLUTION_ROOT="${MY_DIR}/../../.."
+AOSP_ROOT="${MY_DIR}/../../.."
 
-HELPER="${EVOLUTION_ROOT}/vendor/evolution/build/tools/extract_utils.sh"
+HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -43,7 +43,7 @@ done
 
 if [ "$ONLY_DEVICE" == "false" ] && [ -s "${MY_DIR}/proprietary-files.txt" ]; then
     # Initialize the helper for common
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${EVOLUTION_ROOT}" true
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${AOSP_ROOT}" true
 
     # Copyright headers and guards
     write_headers "ginkgo laurel_sprout"
@@ -57,7 +57,7 @@ fi
 
 if [ "$ONLY_COMMON" == "false" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${EVOLUTION_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}" false
 
     # Copyright headers and guards
     write_headers
